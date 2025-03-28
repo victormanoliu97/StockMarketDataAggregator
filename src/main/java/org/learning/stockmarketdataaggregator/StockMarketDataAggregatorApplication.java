fb0ffd6e67eb0cb312266a69910d00908932c0a7
victormanoliu97 <victormanoliu97@gmail.com>
2025-03-29 00:55:28 +0200

    Initial commit

---
+++ src/main/java/org/learning/stockmarketdataaggregator/StockMarketDataAggregatorApplication.java
@@ -0,0 +1,13 @@
+package org.learning.stockmarketdataaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketDataAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketDataAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/learning/stockmarketdataaggregator/StockMarketDataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.stockmarketdataaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketDataAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
